df0c6f1bd6ebdf532e9f4ded7cd5bea5f1189f9f
Alejandra Coeto <a01285221@tec.mx>
2023-07-05 18:31:27 -0600

    UVa problems

---
+++ UVa/00573.cpp
@@ -0,0 +1,76 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <limits.h>
+#include <map>
+#include <math.h>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <utility>
+#include <vector>
+ 
+#define PI 3.141592653589793
+#define EPS 0.000000001
+#define INF 1000000000
+ 
+#define _ ios_base::sync_with_stdio(0), cin.tie(0), cin.tie(0), cout.tie(0), cout.precision(15);
+#define FOR(i, a, b) for(int i=int(a); i<int(b); i++)
+#define RFOR(i, a, b) for(int i=int(a)-1; i>=int(b); i--)
+#define FORC(cont, it) for(typeof((cont).begin()) it = (cont).begin(); it != (cont).end(); it++)
+#define RFORC(cont, it) for(typeof((cont).rbegin()) it = (cont).rbegin(); it != (cont).rend(); it++)
+#define pb push_back
+ 
+using namespace std;
+ 
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+ 
+#define MAXN 10
+#define MOD 1000000007
+ 
+int main() { _
+    
+    int h = 1;
+    int d,u,f;
+    while (h != 0) {
+        
+        cin >> h >> u >> d >> f;
+        if (h==0) break;
+        double distance = 0;
+        double possibleD = u;
+        double factor = u*f*0.01;
+
+        for (int i = 1; distance <= h; i++) {
+            distance += (possibleD > 0) ? possibleD : 0;
+            if (distance > h) {
+                cout << "success on day " << i << endl;
+                break;
+            }
+            possibleD -= factor;
+            distance -= d;
+            if (distance < 0) {
+                cout << "failure on day " << i << endl;
+                break;
+            }
+         
+        }
+
+    }
+ 
+return 0;
+}
---
+++ UVa/11727.cpp
@@ -0,0 +1,63 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <limits.h>
+#include <map>
+#include <math.h>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <utility>
+#include <vector>
+ 
+#define PI 3.141592653589793
+#define EPS 0.000000001
+#define INF 1000000000
+ 
+#define _ ios_base::sync_with_stdio(0), cin.tie(0), cin.tie(0), cout.tie(0), cout.precision(15);
+#define FOR(i, a, b) for(int i=int(a); i<int(b); i++)
+#define RFOR(i, a, b) for(int i=int(a)-1; i>=int(b); i--)
+#define FORC(cont, it) for(typeof((cont).begin()) it = (cont).begin(); it != (cont).end(); it++)
+#define RFORC(cont, it) for(typeof((cont).rbegin()) it = (cont).rbegin(); it != (cont).rend(); it++)
+#define pb push_back
+ 
+using namespace std;
+ 
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+ 
+#define MAXN 10
+#define MOD 1000000007
+ 
+int main() { _
+ int t, a, b, c;
+
+ cin >> t;
+
+ for (int i = 0; i < t; i++) {
+    vector<int> temp;
+    cin >> a >> b >> c; 
+    temp.pb(a);
+    temp.pb(b);
+    temp.pb(c);
+    sort(temp.begin(), temp.end());
+    cout << "Case " << i + 1 << ": " << temp[1] << endl;
+
+ }
+
+return 0;
+}
