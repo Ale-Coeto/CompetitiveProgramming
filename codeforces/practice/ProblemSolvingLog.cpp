b7e570aae739b81c0a029dc58dcc79be10b453fb
Alejandra Coeto <a01285221@tec.mx>
2023-12-31 16:02:44 -0600

    codeforces

---
+++ codeforces/practice/ProblemSolvingLog.cpp
@@ -0,0 +1,78 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <limits.h>
+#include <map>
+#include <math.h>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <utility>
+#include <vector>
+ 
+#define PI 3.141592653589793
+#define EPS 0.000000001
+#define INF 1000000000
+ 
+#define _ ios_base::sync_with_stdio(0), cin.tie(0), cin.tie(0), cout.tie(0), cout.precision(15);
+#define FOR(i, a, b) for(int i=int(a); i<int(b); i++)
+#define RFOR(i, a, b) for(int i=int(a)-1; i>=int(b); i--)
+#define FORC(cont, it) for(typeof((cont).begin()) it = (cont).begin(); it != (cont).end(); it++)
+#define RFORC(cont, it) for(typeof((cont).rbegin()) it = (cont).rbegin(); it != (cont).rend(); it++)
+#define pb push_back
+ 
+using namespace std;
+ 
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+ 
+#define MAXN 10
+#define MOD 1000000007
+ 
+
+int solve(int n, string a) {
+    unordered_map <char, int> freq;
+    for (char letter : a) {
+        freq[letter]++;
+    }
+
+    int ans = 0;
+
+    for (auto i : freq) {
+        if (i.second >= i.first - 'A' + 1)
+            ans++;
+    }
+
+    return ans;
+}
+
+int main() { _
+
+    int t, n;
+
+    cin >> t;
+    string a;
+
+
+    while (t--) {
+        cin >> n;
+        cin >> a;
+        cout << solve(n,a) << endl;
+    }
+
+    return 0;
+}
